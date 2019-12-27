9501ab4367a857c5ea60cd9235af226ac87a16b2
PorisulkiP <48221009+PorisulkiP@users.noreply.github.com>
2019-12-27 19:41:30 +0300

    Add files via upload

---
+++ lib/radio.py
@@ -0,0 +1,6 @@
+'радио дача' : 'http://listen10.vdfm.ru:8000/dacha',
+'русское радио' : 'http://play.russianradio.eu/stream',
+'электронная музыка' : 'http://radio-electron.ru:8000/128',
+'эхо москвы' : 'http://ice912.echo.msk.ru:9120/24.aac',
+'классическая музыка' : 'http://stream.srg-ssr.ch/m/rsc_de/mp3_128',
+'наше радио' : 'http://nashe128.streamr.ru'
